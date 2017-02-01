b012aff9340287e9105e8a00381eb2494c79d974
LeoAristarco <chucky_92@hotmail.com.ar>
2017-02-01 15:52:42 -0300

    Update t-sql.sql

--- t-sql.sql
+++ t-sql.sql
@@ -2,6 +2,8 @@
 --for:se disparan despues de una accion (poniendo for por defecto es after)
 --instead of: se disparan antes de una accion, antes de q ocurra un cambio en las tablas, cancela el resto de las acciones
 
+--en las tablas inserted y deleted, los campos q no se updetean estan los mismos,OSEA NO ESTAN EN NULL
+
 
 /*Implementar el/los objetos necesarios para que no se pueda realizar una factura si el precio de venta de algún artículo
  (ítem_precio) es distinto al precio que se encuentra en la tabla Producto (prod_precio)..*/
@@ -81,24 +83,22 @@ as begin
 
 end
 
-
+------------PUNTO 2---------------
 /*Cree el/los objetos de base de datos necesarios para que se cumpla la siguiente regla de negocio automáticamente
  “Ningún jefe puede tener menos de 5 años de antigüedad y tampoco puede tener más del 50% del personal a su cargo
   (contando directos e indirectos) a excepción del gerente general”. 
   Se sabe que en la actualidad la regla se cumple y existe un único gerente general.*/
 
 
-------------PUNTO 2---------------
 create trigger tr_controlar_jefes on dbo.Empleado
 for insert, update
-as begin transaction
+as begin
+    begin transaction
     
-    declare miCursor cursor for (select empl_codigo, empl_ingreso, empl_jefe from inserted)
+    declare miCursor cursor for (select empl_jefe from inserted)
     
-    declare @cod_nuevo_empleado numeric(6,0)
-    declare @fecha_ingreso smalldatetime
     declare @antiguedad int
-    declare @jefe_nuevo_empleado numeric(6,0)
+    declare @jefe_del_nuevo_empleado numeric(6,0)
     declare @empleadosTotales int
     declare @cantEmpleadosACargo int
     declare @gerenteGeneral int
@@ -111,116 +111,84 @@ as begin transaction
 
 
     OPEN miCursor
-        fetch next from miCursor into @cod_nuevo_empleado, @fecha_ingreso, @jefe_nuevo_empleado
+        fetch next from miCursor into @jefe_del_nuevo_empleado
 
         WHILE(@@FETCH_STATUS = 0)
         begin
-                set @antiguedad = YEAR(@fecha_ingreso)
-                set @cantEmpleadosACargo = (select Count(*) from dbo.Empleado e where loTieneACargo(@jefe_nuevo_empleado, e.empl_codigo) = 1)
 
-                if (@cod_nuevo_empleado <> gerenteGeneral)
+                if (@jefe_del_nuevo_empleado <> @gerenteGeneral)
                     begin
+
+                        set @antiguedad = (select YEAR(empl_ingreso)
+                                            from dbo.Empleado
+                                            where empl_codigo = @jefe_del_nuevo_empleado
+                                           )
+
                         if (@antiguedad < 5)
                             begin 
                                 raiserror('Un jefe no puede tener menos de 5 años de antiguedad',16,1)
                                 rollback transaction
                             end
-                        if (@cantEmpleadosACargo * 100 / @empleadosTotales > 50)
+
+
+                        set @cantEmpleadosACargo =(select Count(*)
+                                                    from dbo.Empleado  
+                                                    where dbo.loTieneACargo(@jefe_del_nuevo_empleado, empl_codigo) = 1
+                                                   )
+
+
+                        if ( (@cantEmpleadosACargo * 100 / @empleadosTotales) > 50)
                             begin
-                                if(@jefe_nuevo_empleado is not null)
+                                if(@jefe_del_nuevo_empleado is not null)
                                     begin
                                     raiserror('Un jefe no puede tener mas del 50% del personal a su cargo', 16,1)
                                     rollback transaction
+                                    end
                             end
                             
                     end
+
+             fetch next from miCursor into @jefe_del_nuevo_empleado
         end
 
     CLOSE miCursor
     DEALLOCATE miCursor
 commit transaction
 end
+
+
 go
 
 
+go
 
-create function loTieneACargo(@cod_jefe numeric(6,0), @cod_empleado numeric(6,0))
-returns bit(1)
+
+create function loTieneACargo(@jefe_supremo numeric(6,0), @cod_empleado numeric(6,0))
+returns bit
 as begin
-    Declare @jefe numeric(6,0)
-    set @jefe = (select empl_jefe From dbo.Empleado e where e.empl_codigo= @cod_empleado)
-    if (@jefe = @cod_jefe)
+    Declare @jefe numeric(6,0), @bool bit
+
+    set @jefe = (select empl_jefe from dbo.Empleado  where empl_codigo= @cod_empleado)
+
+    if (@jefe = @jefe_supremo)
         begin
-            return 1
+            SET @bool=1
         end
     else if(@jefe is null)
         begin
-            return 0
+            SET @bool=0
         end
     else
         begin
-            return loTieneACargo(@cod_jefe, @jefe)
+            SET @bool= dbo.loTieneACargo(@jefe_supremo, @jefe)
         end
+
+    return @bool 
 end
 go
 
--- recuperatorio 28/06/2016
-/* 
-En virtud de una recategorizacion de productos referida a la familia de los mismos se solicita que desarrolle una consulta sql
-que retorne para todos los productos: 
-1- Codigo de producto
-2- Detalle del producto 
-3- Codigo de la familia del producto 
-4- Detalle de la familia actual del producto
-5- Codigo de la familia sugerido para el producto 
-6- Detalla de la familia sugerido para el producto 
- 
-La familia sugerida para un producto es la que poseen la mayoria de los productos cuyo detalle coinciden en los primeros 5 caracteres.
-En caso que 2 o mas familias pudieran ser sugeridas se debera seleccionar la de menor codigo.
-Solo se deben mostrar los productos para los cuales la familia actual sea diferente a la sugerida
-Los resultados deben ser ordenados por detalle de producto de manera ascendente 
-*/
+-- esto nose que es ?¿
 
-SELECT P.prod_codigo AS codProducto, P.prod_detalle AS detalleProducto, 
-    F.fami_id AS codFamiliaActualProducto, F.fami_detalle AS detalleFamiliaActualProducto,
-    F2.fami_id AS codFamiliaSugeridaProducto, F2.fami_detalle AS detalleFamiliaSugeridaProducto
-FROM Producto P
-inner join Familia F on F.fami_id = P.prod_familia ,
-Familia F2
-where F2.fami_id = (SELECT TOP 1 Fam.fami_id
-                    FROM Producto Prod
-                    inner join Familia Fam on Fam.fami_id = Prod.prod_familia
-                    WHERE SUBSTRING(Prod.prod_detalle, 1, 5) = SUBSTRING(P.prod_detalle, 1, 5)
-                    GROUP BY Fam.fami_id 
-                    ORDER BY Count(DISTINCT Prod.prod_codigo) DESC, Fam.fami_id ASC)
-AND P.prod_familia != F2.fami_id
-ORDER BY P.prod_detalle ASC
-
-
-/*
-. Escriba una consulta sql que retorne para todos los años, en los cuales se haya hecho al menos una factura,
-la cantidad de clientes a los que se les facturo de manera incorrecta al menos una factura y 
-que cantidad de facturas se realizaron de manera incorrecta.
-Se considera que una factura es incorrecta cuando la diferencia
-entre el total de la factura menos el total de impuesto tiene una diferencia mayor
-a $ 1 respecto a la sumatoria de los costos de cada uno de los items de dicha factura
-. Las columnas que se deben mostrar son: 
-1- Año
-2- Clientes a los que se les facturo mal en ese año
-3- Facturas mal realizadas en ese año 
-*/
-
-select YEAR(f1.fact_fecha) as Año, COUNT(DISTINCT f1.fact_cliente), COUNT(*)
-from Factura f1
-   WHERE ((f1.fact_total - f1.fact_total_impuestos) - ( select SUM(item_precio * item_cantidad) from Item_Factura
-     where item_tipo=f1.fact_tipo and item_sucursal=f1.fact_sucursal and item_numero=f1.fact_numero) > 1 )
-group by YEAR(f1.fact_fecha)
-HAVING COUNT(DISTINCT f1.fact_fecha) >= 1
-
-
-/*
-
-*/
 create trigger tg_jefes_de_no_mas_de_10_empleados
 on Empleado
 after insert, delete
@@ -298,3 +266,16 @@ as begin
 
 
 end
+
+
+/*
+en teoria este ejercicio esta bien, el enunciado dice q "cada vez que se decida incrementar la comision
+de un empleado no se permita incrementar mas de un 5 %", por eso no contemple la posibilidad q cambien otros atributos
+osea, imagine que dichos cambios se harian de este modo    [update Empleado  SET empl_comision = 4 where empl_codigo=3].
+
+se que  Al hacer un trigger instead of, si no modifica los datos en el codigo no se hace. Por eso es q hago 
+                                [update Empleado  SET empl_comision = @comisionNueva
+                                where empl_codigo=@empl_codigo] dentro del codigo del trigger
+en definitiva cumplo con lo que dice el enunciado
+
+*/
